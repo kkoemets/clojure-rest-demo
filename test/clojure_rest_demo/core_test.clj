e803a9a9b720a9df769c229f983323d669a82fdd
Kristjan Koemets <koemets.kristjan@gmail.com>
2019-12-02 20:51:54 +0200

    Initialize

---
+++ test/clojure_rest_demo/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-rest-demo.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-rest-demo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
